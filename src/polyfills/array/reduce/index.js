2a89da37c674c05a14286ef73f6efe9d53fe42cf
Divyansh <divyansh.yadav@gmail.com>
2021-08-08 10:42:13 +0530

    add yarn lock | add reduce fun imp

--- src/polyfills/array/reduce/index.js
+++ src/polyfills/array/reduce/index.js
@@ -1,5 +1,10 @@
 function reduce(array, fn, initialValue) {
+    let value = initialValue
+    array.forEach(item => {
+        value = fn(value, item)
+    })
 
+    return value
 }
 
 export default reduce;
