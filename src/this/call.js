950c2149fcbc795159d06837619e806c37752006
Divyansh <divyansh.yadav@gmail.com>
2022-05-02 12:42:37 +0100

    add lint config

--- src/this/call.js
+++ src/this/call.js
@@ -1,12 +1,12 @@
 const getUniqueId = () => `${new Date().getTime()}${Math.random()}`;
 function call(context, fn, ...args) {
-    let id = getUniqueId();
-    while (context.hasOwnProperty(id)) {
-        id = getUniqueId();
-    }
+	let id = getUniqueId();
+	while (Object.prototype.hasOwnProperty.call(context, id)) {
+		id = getUniqueId();
+	}
 
-    context[id] = fn;
-    return context[id](...args);
+	context[id] = fn;
+	return context[id](...args);
 }
 
 module.exports = call;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line import/prefer-default-export
-export const wait = (ms, fulfill = true) => new Promise((res, rej) => {
-    setTimeout(() => {
-        if (fulfill) {
-            res(ms);
-        }
-        rej(ms);
-    }, ms);
-});
+export const wait = (ms, fulfill = true) =>
+	new Promise((res, rej) => {
+		setTimeout(() => {
+			if (fulfill) {
+				res(ms);
+			}
+			rej(ms);
+		}, ms);
+	});
